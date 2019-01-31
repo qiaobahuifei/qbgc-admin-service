2d8be5beb3ba2a5249a3cd2b477a65777a2bbd95
gaochao <gaochao5kuba@163,com>
2019-01-31 10:56:15 +0800

    放到admin组中

---
+++ src/main/java/com/qbgc/admin/service/QbgcAdminServiceApplication.java
@@ -0,0 +1,14 @@
+package com.qbgc.admin.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbgcAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbgcAdminServiceApplication.class, args);
+	}
+
+}
+
