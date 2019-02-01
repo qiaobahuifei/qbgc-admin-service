6303cec2bead39c206a445b6f63020d5fec7e9ce
gaochao <gaochao5kuba@163,com>
2019-02-01 10:14:20 +0800

    注释调引过来的数据库依赖

--- src/main/java/com/qbgc/admin/service/Application.java
+++ src/main/java/com/qbgc/admin/service/Application.java
@@ -3,6 +3,7 @@ package com.qbgc.admin.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author gaochao
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @RestController
 @EnableEurekaClient
 public class Application {
@@ -21,6 +22,7 @@ public class Application {
 	}
 
 	@Value("${server.port}")
+	private
 	String port;
 
 	@RequestMapping("/hi")
